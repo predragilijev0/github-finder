f08bed712f5fa60d6740ad64ebeb1347e075413a
Predrag Ilijev <predragilijev@gmail.com>
2022-05-30 04:18:27 +0200

    Routing to Home, About and NotFound

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
 
     return (
         <footer className="footer p-10 bg-gray-700 text-primary-content footer-center">
-            <p>Copyright &copy; {footerYear}</p>
+            <p>Copyright &copy; {footerYear} All rights reserved</p>
         </footer>
     )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div>
+            <div className="text-6xl">Welcome</div>
+        </div>
+    )
+}
+
+export default Home
