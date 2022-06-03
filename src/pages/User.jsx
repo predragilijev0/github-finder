e30ee4141b36d543d0ba9140b3a8ddbc077a4133
Predrag Ilijev <predragilijev@gmail.com>
2022-06-03 18:17:20 +0200

    Added Repos to user profiles

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -3,14 +3,17 @@ import { useEffect, useContext } from 'react'
 import GithubContext from '../context/github/GithubContext'
 import { useParams, Link } from 'react-router-dom'
 import Spinner from '../components/layout/Spinner'
+import RepoList from '../components/repos/RepoList'
 
 function User() {
-    const { getUser, user, loading } = useContext(GithubContext)
+    const { getUser, user, loading, getUserRepos, repos } =
+    useContext(GithubContext)
 
     const params = useParams()
 
     useEffect(() => {
         getUser(params.login)
+        getUserRepos(params.login)
     }, [])
 
     const {
@@ -159,6 +162,8 @@ function User() {
                         </div>
                     </div>
                 </div>
+
+                <RepoList repos={repos} />
             </div>
         </>
     )
